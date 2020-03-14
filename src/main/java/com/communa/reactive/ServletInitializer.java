17bad83e1b61667e4cfd536a0a942afb7b497f0b
vishal <vishalsinghyadav565@gmail.com>
2020-03-14 23:56:59 +0530

    first commit

---
+++ src/main/java/com/communa/reactive/ServletInitializer.java
@@ -0,0 +1,14 @@
+package com.communa.reactive;
+
+import com.communa.reactive.CommunaApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(CommunaApplication.class);
+	}
+
+}
---
+++ src/test/java/com/communa/reactive/CommunaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.communa.reactive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
