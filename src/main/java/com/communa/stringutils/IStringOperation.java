4ebf0e09cef0f5e9d454446fd38cf71bfb8267db
vishal <vishalsinghyadav565@gmail.com>
2020-03-18 20:30:24 +0530

    added comments in interface

--- src/main/java/com/communa/stringutils/IStringOperation.java
+++ src/main/java/com/communa/stringutils/IStringOperation.java
@@ -7,7 +7,21 @@ import java.util.Map;
 
 public interface IStringOperation {
 
+   /**
+    *  This checks the content of the string and returns true if equals
+    * @param string1
+    * @param string2
+    * @return boolean
+    * @throws Exception
+    */
    boolean stringEquals(String string1, String string2) throws Exception;
 
+   /**
+    * This checks the unequality of the strings using recursion methodology
+    * and returns the list of unmatched string.
+    * @param jsonNode1
+    * @param jsonNode2
+    * @return List<Map<String, String>>
+    */
    List<Map<String, String>> stringUnequals(JsonNode jsonNode1, JsonNode jsonNode2);
 }
